43a076b72b49ca8d0279ee11ea90da713478db26
Alex <achenriques@esei.uvigo.es>
2020-11-16 17:45:42 +0100

    Se corrige falla de carga de imagenes en pro 3

--- ReadooRestProvider/util/serverOptions.js
+++ ReadooRestProvider/util/serverOptions.js
@@ -1,4 +1,4 @@
-const uploadAvatarDir = (process.env.NODE_ENV === 'production') ? './uploads/userAvatars' : './ReadooRestProvider/uploads/userAvatars';
+const uploadAvatarDir = (process.env.NODE_ENV === 'production') ? '../uploads/userAvatars' : './ReadooRestProvider/uploads/userAvatars';
 const uploadCoverDir = (process.env.NODE_ENV === 'production') ? '../uploads/coverPages' : './ReadooRestProvider/uploads/coverPages';
 
 module.exports = { uploadAvatarDir, uploadCoverDir };
