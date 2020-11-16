c28e2538cfa18087efd69ad83b0e1d70ea0203bb
Alex <achenriques@esei.uvigo.es>
2020-11-16 11:33:02 +0100

    Anado herokuapp a lista hosts para cors

--- ReadooRestProvider/util/corsWhitelist.js
+++ ReadooRestProvider/util/corsWhitelist.js
@@ -2,5 +2,7 @@ module.exports = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:3030',
-    'http://127.0.0.1:3030'
+    'http://127.0.0.1:3030',
+    'http://readoo.herokuapp.com/',
+    'https://readoo.herokuapp.com/'
 ]
